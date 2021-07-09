8f2d426d6b9a0ab3200b7a9530618fbc9d6e3246
Omar Mohamed <60580509+omarfawzy1@users.noreply.github.com>
2021-07-09 20:10:24 +0200

    BookBrowser
    Allowing the Books To come out from the Data Base
    
    Co-Authored-By: ozoo500 <85310781+ozoo500@users.noreply.github.com>

--- project/Books/models.py
+++ project/Books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.fields import CharField
+
+
+
+class Book (models.Model):
+        title = CharField(max_length=100, default="")
+        authorName = CharField(max_length=100)
+        cover = models.ImageField(upload_to = 'covers', default="")
 
 # Create your models here.
--- project/Books/views.py
+++ project/Books/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Book
 # Create your views here.
 
 def index(request):
@@ -7,6 +8,9 @@ def index(request):
 
 def about(request):
     return render(request,'Books/about.html')
+
+def bookInfo(request):
+    return render(request,'Books/BooksBrowser.html',{'books' : Book.objects.all()})
 
 
 
